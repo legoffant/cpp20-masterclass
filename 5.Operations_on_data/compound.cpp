72c0b5c93916da84c5e1fb56b7fc38d5c0a09e3c
legoffant <legoff.ant@gmail.com>
2022-03-02 08:56:48 +0100

    add compound ioformat

---
+++ 5.Operations_on_data/compound.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+
+    int value {45};
+
+    value +=5; // equivalent value = value + 5
+
+    std::cout << "valeur: " << value << std::endl;
+
+    return 0;
+}
